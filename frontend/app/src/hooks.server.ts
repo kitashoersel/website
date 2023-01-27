8d2752b86809c13076393f6c6b633aff410302ee
DevTobias <tobi.kaerst@gmx.de>
2023-01-27 17:12:25 +0100

    feat: hardened security headers

--- frontend/app/src/hooks.server.ts
+++ frontend/app/src/hooks.server.ts
@@ -3,12 +3,50 @@ import { initAcceptLanguageHeaderDetector } from 'typesafe-i18n/detectors';
 import { detectLocale, i18n, isLocale } from '$i18n/i18n-util';
 import { loadAllLocales } from '$i18n/i18n-util.sync';
 
+const rootDomain = 'localhost:3000';
+
 loadAllLocales();
 const L = i18n();
 
 const getPreferredLocale = ({ request }: RequestEvent) => {
   const acceptLanguageDetector = initAcceptLanguageHeaderDetector(request);
   return detectLocale(acceptLanguageDetector);
+};
+
+const setSecurityHeaders = (response: Response) => {
+  const cspDirectives = {
+    'base-uri': ["'self'"],
+    'child-src': ["'self'"],
+    'connect-src': ["'self'", 'ws://localhost:*', 'https://hcaptcha.com', 'https://*.hcaptcha.com'],
+    'img-src': ["'self'", 'data:'],
+    'font-src': ["'self'", 'data:'],
+    'form-action': ["'self'"],
+    'frame-ancestors': ["'self'"],
+    'frame-src': ["'self'", 'https://hcaptcha.com', 'https://*.hcaptcha.com'],
+    'manifest-src': ["'self'"],
+    'media-src': ["'self'", 'data:'],
+    'object-src': ["'none'"],
+    'style-src': ["'self'", "'unsafe-inline'", 'https://hcaptcha.com', 'https://*.hcaptcha.com'],
+    'default-src': ["'self'", rootDomain, `ws://${rootDomain}`],
+    'script-src': ["'self'", "'unsafe-inline'", 'https://hcaptcha.com', 'https://*.hcaptcha.com'],
+    'worker-src': ["'self'"],
+  };
+
+  const csp = Object.entries(cspDirectives)
+    .map(([key, arr]) => `${key} ${arr.join(' ')}`)
+    .join('; ');
+
+  response.headers.set('X-Frame-Options', 'SAMEORIGIN');
+  response.headers.set('Referrer-Policy', 'no-referrer');
+  response.headers.set(
+    'Permissions-Policy',
+    'accelerometer=(), autoplay=(), camera=(), document-domain=(), encrypted-media=(), fullscreen=(), gyroscope=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), sync-xhr=(), usb=(), xr-spatial-tracking=(), geolocation=()'
+  );
+  response.headers.set('X-Content-Type-Options', 'nosniff');
+  response.headers.set('Content-Security-Policy', csp);
+  response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+
+  return response;
 };
 
 export const handle: Handle = async ({ event, resolve }) => {
@@ -29,5 +67,9 @@ export const handle: Handle = async ({ event, resolve }) => {
   event.locals.locale = locale;
   event.locals.LL = LL;
 
-  return resolve(event, { transformPageChunk: ({ html }) => html.replace('%lang%', locale) });
+  const response = await resolve(event, {
+    transformPageChunk: ({ html }) => html.replace('%lang%', locale),
+  });
+
+  return setSecurityHeaders(response);
 };
