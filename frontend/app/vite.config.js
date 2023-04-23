af52ad5391ae14d377381df35b86a4737eda1ed4
Tobias Kärst <tobias.kaerst@qbeyond.de>
2023-04-23 12:47:14 +0000

    feat: imagetools integration

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-// import { imagetools } from '@zerodevx/svelte-img/vite';
+import { imagetools } from '@modules/svelte-image';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-  plugins: [sveltekit()], // imagetools()],
+  plugins: [sveltekit(), imagetools()],
   server: { port: 3000 },
   preview: { port: 3000 },
   css: {
---
+++ frontend/modules/svelte-image/index.js
@@ -0,0 +1,50 @@
+import { imagetools } from "vite-imagetools";
+import {
+  resize,
+  format,
+  setMetadata,
+  metadataFormat,
+  resolveConfigs,
+} from "imagetools-core";
+
+function lqip(cfg, ctx) {
+  if (cfg.lqip) {
+    const r = resize({ width: cfg.lqip }, ctx);
+    const f = format({ format: "webp", quality: "20" }, ctx);
+    return async function (image) {
+      const img = f(r(image));
+      const buffer = await img.toBuffer();
+      setMetadata(
+        img,
+        "base64",
+        `data:image/webp;base64,${buffer.toString("base64")}`
+      );
+      return img;
+    };
+  }
+}
+
+function main(overrides = {}) {
+  return imagetools({
+    defaultDirectives: () =>
+      new URLSearchParams("width=480;1024;1920&format=avif;webp;jpg"),
+    extendTransforms: (builtins) => [...builtins, lqip],
+    extendOutputFormats: (builtinOutputFormats) => ({
+      ...builtinOutputFormats,
+      run: () => metadataFormat(["format", "src", "width", "height", "base64"]),
+    }),
+    resolveConfigs: (e, f) => {
+      if (e.findIndex((i) => i[0] === "run") > -1) {
+        const idx = e.findIndex((i) => i[0] === "lqip");
+        const lqip = idx > -1 ? parseInt(e.splice(idx, 1)[0][1][0]) : 16;
+        const merge = new Map();
+        for (const [key, val] of e) merge.set(key, val);
+        return [...resolveConfigs([...merge], f), ...(lqip ? [{ lqip }] : [])];
+      }
+      return resolveConfigs(e, f);
+    },
+    ...overrides,
+  });
+}
+
+export { main as imagetools, lqip };
