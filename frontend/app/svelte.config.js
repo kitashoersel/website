0b9a1d7a111af1fdf1548a359588699762a0e51b
Tobias Kärst <tobi.kaerst@gmx.de>
2023-05-06 15:50:11 +0000

    feat: better csp styles rules

--- frontend/app/svelte.config.js
+++ frontend/app/svelte.config.js
@@ -27,7 +27,6 @@ const config = {
         'style-src': ['self', 'unsafe-inline'],
         'script-src': ['self', 'unsafe-inline'],
         'script-src-attr': ['none'],
-        'style-src': ['self'],
         'form-action': ['self'],
         'frame-ancestors': ['self'],
         'img-src': ['self', 'admin.kitashoersel.de', 'data:'],
