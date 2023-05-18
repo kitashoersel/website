62ee43eeaf832c02118a26c957e7ab2caadb924e
Tobias Kärst <tobi.kaerst@gmx.de>
2023-05-18 20:03:02 +0000

    feat: deployment config

--- frontend/app/src/lib/config.ts
+++ frontend/app/src/lib/config.ts
@@ -5,8 +5,8 @@ export const config = {
   siteBackgroundColor: '#f6f6f9',
   release: new Date(2023, 2, 1, 1, 0),
 
-  contactEmail: env.PUBLIC_CONTACT_EMAIL,
-  siteUrl: env.PUBLIC_SITE_URL,
-  cmsEndpoint: env.PUBLIC_CMS_ENDPOINT,
-  assetEndpoint: env.PUBLIC_ASSET_ENDPOINT,
+  contactEmail: env.PUBLIC_CONTACT_EMAIL!,
+  siteUrl: env.PUBLIC_SITE_URL!,
+  cmsEndpoint: env.PUBLIC_CMS_ENDPOINT!,
+  assetEndpoint: env.PUBLIC_ASSET_ENDPOINT!,
 };
